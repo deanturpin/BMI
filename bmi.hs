05cbd2048cd80d12c4bf22e78413649f052ca861
deanturpin <git@lvbtn.uk>
2016-07-19 09:20:39 +0100

    use putStrLn instead of print

--- bmi.hs
+++ bmi.hs
@@ -1,6 +1,12 @@
 import System.Environment (getArgs)
 
+height = 1.82
+bmi weight = div weight (height * height)
+
 main = do
+	putStrLn "BMI calc"
 	args <- getArgs
-	print $ args!!0
-	print $ args!!1
+	putStrLn $ args!!0
+	putStrLn $ (bmi 75.7 1.82)
+	-- print $ read $ args!!1 :: Float
+	-- print $ bmi $ read args!!0 :: Float
