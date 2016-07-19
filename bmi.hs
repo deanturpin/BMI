b1e94e8c0162d3d4f666e604246b490657f013d8
deanturpin <git@lvbtn.uk>
2016-07-19 09:27:22 +0100

    add prototype for bmi

--- bmi.hs
+++ bmi.hs
@@ -1,12 +1,15 @@
 import System.Environment (getArgs)
 
-height = 1.82
-bmi weight = div weight (height * height)
+height = 1.82 :: Double
+bmi :: Double -> Double
+bmi weight = weight / (height * height)
 
 main = do
 	putStrLn "BMI calc"
 	args <- getArgs
 	putStrLn $ args!!0
-	putStrLn $ (bmi 75.7 1.82)
+	putStrLn $ head args
+	print $ bmi 75.7
+	-- putStrLn $ bmi 75.7
 	-- print $ read $ args!!1 :: Float
 	-- print $ bmi $ read args!!0 :: Float
