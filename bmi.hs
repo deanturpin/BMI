93a509f838066dfeb3b91e372b6e741a11638e71
deanturpin <git@lvbtn.uk>
2016-07-21 18:32:03 +0100

    convert command line param to double

--- bmi.hs
+++ bmi.hs
@@ -7,11 +7,5 @@ bmi :: Double -> Double
 bmi mass = mass / (height * height)
 
 main = do
-	putStrLn "BMI calc"
 	args <- getArgs
-	putStrLn $ "mass in " ++ args!!0
-	putStrLn $ head args
-	print $ bmi 75.7
-	-- putStrLn $ bmi 75.7
-	-- print $ read $ args!!1 :: Float
-	-- print $ bmi $ read args!!0 :: Float
+	print $ bmi (read (head args) :: Double)
