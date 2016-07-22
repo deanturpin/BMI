49f53123be56bf32cb53ffa0da6a31f55f1ae099
deanturpin <git@lvbtn.uk>
2016-07-22 15:02:03 +0100

    calculate tyre pressures

--- bmi.hs
+++ bmi.hs
@@ -6,7 +6,21 @@ height = 1.82 :: Double
 bmi :: Double -> Double
 bmi mass = mass / (height * height)
 
+-- Optimal tyre pressures are proportional to the combined weight of the rider
+-- and bike
+--  let pressure weight = weight * (55/71) + 48
+bikeMass = 11
+pressure riderMass loading = (((riderMass + bikeMass) * loading) * (55 / 71)) + 48
+
+-- Read params
+-- Calculate BMI
+-- Calculate optimum tyre pressures
 main = do
 	args <- getArgs
 	print $ bmi (read (head args) :: Double)
+	putStr "BMI "
 	print $ (25 + 18.5) / 2
+	putStr "Front pressure "
+	print $ pressure (read (head args) :: Double) 0.4
+	putStr "Rear pressure "
+	print $ pressure (read (head args) :: Double) 0.6
