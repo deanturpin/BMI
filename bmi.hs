984b45f98e44eedaaba9c1a02dd555e82ed91847
deanturpin <git@lvbtn.uk>
2016-07-22 15:09:02 +0100

    report loading of front and rear

--- bmi.hs
+++ bmi.hs
@@ -6,9 +6,9 @@ height = 1.82 :: Double
 bmi :: Double -> Double
 bmi mass = mass / (height * height)
 
--- Optimal tyre pressures are proportional to the combined weight of the rider
--- and bike
---  let pressure weight = weight * (55/71) + 48
+-- Calculate tyre pressures
+-- Pressures proportional to combined mass of the rider and bike
+-- Assumes 25mm tyre
 bikeMass = 11
 pressure riderMass loading = (((riderMass + bikeMass) * loading) * (55 / 71)) + 48
 
@@ -20,7 +20,11 @@ main = do
 	print $ bmi (read (head args) :: Double)
 	putStr "BMI "
 	print $ (25 + 18.5) / 2
+	putStr "Front mass "
+	print $ (bikeMass + (read (head args)) :: Double) * 0.4
 	putStr "Front pressure "
 	print $ pressure (read (head args) :: Double) 0.4
+	putStr "Rear mass "
+	print $ (bikeMass + (read (head args) :: Double)) * 0.6
 	putStr "Rear pressure "
 	print $ pressure (read (head args) :: Double) 0.6
